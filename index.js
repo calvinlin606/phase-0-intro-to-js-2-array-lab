6c84c42ba25ec739aa49293e0659d8642e595380
Calvin <calvinlin6@gmail.com>
2022-12-26 16:25:03 -0500

    commit tree 253cacc53c9904e8ec01872b3e111d20d1cadfc2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){cats.push('Ralph');}
+function destructivelyPrependCat(){cats.unshift('Bob');}
+function destructivelyRemoveLastCat(){cats.pop('Ralph');}
+function destructivelyRemoveFirstCat(){cats.shift('Bob');}
+function appendCat(){const copyofCats=[...cats, "Broom"];return copyofCats}
+function prependCat(){const copyofCats=["Arnold", ...cats];return copyofCats}
+function removeLastCat(){const newCats=cats.slice(0, array.length -1);return newCats}
+cats.length = 0;
+
+      cats.push('Milo', 'Otis', 'Garfield');
+      function removeLastCat(){const copyofCats=cats.slice(2, array.length -1);return copyofCats}
